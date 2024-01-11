d8ba761d9781c8af440796a4c2e144feeab623f4
K.V.Y.S. Kumara <86467727+Yasindu-Kumara@users.noreply.github.com>
2024-01-11 11:32:31 +0530

    starter-project

---
+++ Backend/server.js
@@ -0,0 +1,16 @@
+const dotenv = require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+//connect to mongoDB
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => {
+        app.listen(port, () => {console.log(`Server is running on port: ${port}`)});
+    })
+.catch(err => console.log(err));
